96d08ab112ef97651ea71565e9bda8102701cca6
Salman Abuhaimed <salman.f.abuhaimed@gmail.com>
2025-11-06 23:14:35 +0300

    add side bar to home page

--- web/src/pages/home.rs
+++ web/src/pages/home.rs
@@ -1,15 +1,42 @@
 use dioxus::prelude::*;
 
-use crate::{config, modules::tree::components::tree::Tree};
+use crate::{Route, config, modules::tree::components::tree::Tree};
+
+#[component]
+fn Sidebar(show_sidebar: Signal<bool>) -> Element {
+    rsx! {
+        div {
+            class: if !show_sidebar() {
+                "hidden"
+            },
+            class: "flex flex-col fixed right-0 z-1 bg-(--color-forest-background) h-full px-8 py-5 space-y-10",
+
+            Link { to: Route::Home {}, "الصفحة الرئيسية" }
+        }
+    }
+}
 
 #[component]
 pub fn Home() -> Element {
     let config = use_context::<config::client::Config>();
+    let mut show_sidebar = use_signal(|| true);
 
     rsx! {
         div {
-            class: "h-full w-full",
+            class: "h-full w-full p-3",
             dir: "rtl",
+            div {
+                button {
+                    class: "btn p-5",
+                    onclick: move |_| {
+                        show_sidebar.with_mut(|show_sidebar| {
+                            *show_sidebar = !*show_sidebar;
+                        });
+                    },
+                    "<"
+                }
+                Sidebar { show_sidebar }
+            }
             div {
                 class: "p-10",
                 h1 {
